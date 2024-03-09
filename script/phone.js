2c2ac7e37948582ffbe82d1f5d329fb4c37af1c7
Priyo0011 <priyotosh0011@gmail.com>
2024-03-09 23:31:38 +0600

    search ber showall btn loading spinner

--- script/phone.js
+++ script/phone.js
@@ -1,19 +1,31 @@
-const loadPhone = async () => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
-  displayPhones(phones);
+  displayPhones(phones,isShowAll);
 };
 
-const phoneContainer = document.getElementById('phone-container')
+const displayPhones = (phones, isShowAll) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phoneContainer.textContent = '';
 
-const displayPhones = (phones) => {
+  const showAllContainer = document.getElementById('show-all-container');
+  if(phones.length > 12 && !isShowAll){
+    showAllContainer.classList.remove('hidden');
+  }else{
+    showAllContainer.classList.add('hidden');
+  }
+
+  if(!isShowAll){
+    phones =phones.slice(0,12);
+  }
+
   phones.forEach((phone) => {
     console.log(phone);
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card bg-base-100 p-6 shadow-xl`;
+    phoneCard.classList = `card bg-base-100 p-6 shadow-xl mt-8`;
     phoneCard.innerHTML = `
     <figure><img src="${phone.image}"
     alt="" /></figure>
@@ -27,6 +39,27 @@ const displayPhones = (phones) => {
     `;
     phoneContainer.appendChild(phoneCard);
   });
+  toggleLoadingSpinner(false);
 };
 
-loadPhone();
+const handleSearch = (isShowAll) => {
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadPhone(searchText, isShowAll);
+};
+
+const toggleLoadingSpinner = (isLoading) =>{
+  const loadingSpinner = document.getElementById('loading-spinner');
+  if(isLoading){
+    loadingSpinner.classList.remove('hidden');
+  }else{
+    loadingSpinner.classList.add('hidden');
+  }
+}
+
+const handleShowAll = () => {
+  handleSearch(true);
+}
+
+loadPhone("iphone");
